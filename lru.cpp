ba7687a222e1b8d536719469c9f113667d0fed10
Scott Walls <scottwalls2008@gmail.com>
2020-06-20 16:56:55 +0000

    LRU Cache Class
    A functional implementation of the solution to the LeetCode LRU cache problem.

---
+++ lru.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class LRUCache {
+    int size_;
+    unordered_map<int, int> ma;
+    list<int> lst;
+public:
+    LRUCache(int capacity) : size_(capacity) {
+        ma.clear();
+        lst.clear();
+    }
+    
+    int get(int key) {
+        //not found
+        if (ma.find(key) == ma.end()){
+            return -1;
+        }
+        //found
+        else {
+            list<int>::iterator it = lst.begin();
+            while (*it != key) {
+                it++;
+            }
+            lst.erase(it);
+            lst.push_front(key);
+            return ma[key];
+        }
+    }
+    
+    void put(int key, int value) {
+        //found
+        if (ma.find(key)!= ma.end()) {
+            list<int>::iterator it = lst.begin();
+            while (*it != key) {
+                it++;
+            }
+            lst.erase(it);
+            lst.push_front(key);
+            ma[key] = value;
+        }
+        //not found
+        else {
+            //cache full
+            if (size_ == lst.size()) {
+                int back = lst.back();
+                lst.pop_back();
+                ma.erase(back);
+            }
+            lst.push_front(key);
+            ma[key] = value;
+        }
+    }
+};
